2c783712a642333167db947f7a3932fe30f76874
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-08-20 11:07:27 +0200

    initial commit

--- src/sfc.d.ts
+++ src/sfc.d.ts
@@ -1,4 +1,4 @@
-// Para decirle a TypeScript cómo usar SFC
+// Para decirle a TypeScript cómo usar SFC (Single File Component)
 declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
